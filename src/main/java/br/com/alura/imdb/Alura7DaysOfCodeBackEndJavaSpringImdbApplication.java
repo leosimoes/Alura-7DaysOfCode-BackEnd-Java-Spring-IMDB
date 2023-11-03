64ed60ffcce6268366be0ac0d9e0a7513db54810
Leonardo <leosimoes.engcomp@gmail.com>
2023-11-03 12:36:21 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/alura/imdb/Alura7DaysOfCodeBackEndJavaSpringImdbApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alura7DaysOfCodeBackEndJavaSpringImdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Alura7DaysOfCodeBackEndJavaSpringImdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/alura/imdb/Alura7DaysOfCodeBackEndJavaSpringImdbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alura7DaysOfCodeBackEndJavaSpringImdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
